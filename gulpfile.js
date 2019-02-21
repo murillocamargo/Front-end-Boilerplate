a8685dbe32b36520df8ca7446e203d5107751539
Murillo Camargo <murillo@murillocamargo.com.br>
2019-02-20 16:37:25 -0800

    Improve config

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,114 @@
 'use strict';
 
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
 
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./_site/"
+        },
+        port: 3000
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+// Clean assets
+function clean() {
+    return del(["./_site/assets/"]);
+}
+
+// Optimize Images
+function images() {
+    return gulp
+        .src("./assets/img/**/*")
+        .pipe(newer("./_site/assets/img"))
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({interlaced: true}),
+                imagemin.jpegtran({progressive: true}),
+                imagemin.optipng({optimizationLevel: 5}),
+                imagemin.svgo({
+                    plugins: [
+                        {
+                            removeViewBox: false,
+                            collapseGroups: true
+                        }
+                    ]
+                })
+            ])
+        )
+        .pipe(gulp.dest("./_site/assets/img"));
+}
+
+// CSS task
 function css() {
     return gulp
-        .src('./assets/scss/**/*.scss')
+        .src("./assets/scss/**/*.scss")
         .pipe(plumber())
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./_site/assets/css/'))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(sass({outputStyle: "expanded"}))
+        .pipe(gulp.dest("./_site/assets/css/"))
+        .pipe(rename({suffix: ".min"}))
         .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(gulp.dest('./_site/assets/css/'));
+        .pipe(gulp.dest("./_site/assets/css/"))
+        .pipe(browsersync.stream());
 }
 
-const compileMarkup = () => {
-};
-
-const compileScript = () => {
-};
-
-const compileStyle = () => {
-};
-
-const watchMarkup = () => {
-};
-
-const watchScript = () => {
-};
-
-const watchStyle = () => {
-};
-
-const compile = gulp.parallel(compileMarkup, compileScript, compileStyle);
-compile.description = 'compile all sources';
-
-const startServer = () => {
-};
-
-const serve = gulp.series(compile, startServer);
-serve.description = 'serve compiled source on local server at port 3000';
-
-const watch = gulp.parallel(watchMarkup, watchScript, watchStyle);
-watch.description = 'watch for changes to all source';
-
-const defaultTasks = gulp.parallel(serve, watch);
-
-export {
-    compile,
-    compileMarkup,
-    compileScript,
-    compileStyle,
-    serve,
-    watch,
-    watchMarkup,
-    watchScript,
-    watchStyle,
+// Lint scripts
+function scriptsLint() {
+    return gulp
+        .src(["./assets/js/**/*", "./gulpfile.js"])
+        .pipe(plumber())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 }
 
-export default defaultTasks
+// Watch files
+function watchFiles() {
+    gulp.watch("./assets/scss/**/*", css);
+    gulp.watch("./assets/js/**/*", gulp.series(scriptsLint, scripts));
+    gulp.watch(
+        [
+            "./_includes/**/*",
+            "./_layouts/**/*",
+            "./_pages/**/*",
+            "./_posts/**/*",
+            "./_projects/**/*"
+        ],
+        gulp.series(browserSyncReload)
+    );
+    gulp.watch("./assets/img/**/*", images);
+}
+
+// define complex tasks
+const js = gulp.series(scriptsLint, scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
